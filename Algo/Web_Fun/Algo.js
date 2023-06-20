7d5ee8c0c78e0d8c2dfb17a41a92219ddeb9e625
fatmagh96 <fghrab@gmail.com>
2023-06-20 10:19:38 +0100

    .\Algo.js

--- Algo/Web_Fun/Algo.js
+++ Algo/Web_Fun/Algo.js
@@ -13,6 +13,13 @@ let timeTaken = Date.now() - start;
 console.log("Total time taken : " + timeTaken + " milliseconds");
 
 
+// Algo Exercices
+
+// https://docs.google.com/spreadsheets/d/1AuGt0ihsR6_uSLfx3mwpYiCrKVp2DdLqN16ccaYxfMU/edit#gid=462018514
+
+
+
+
 // for (var i=0;i<10;i++){
 //     console.log(i);
 //     i = i+3;
